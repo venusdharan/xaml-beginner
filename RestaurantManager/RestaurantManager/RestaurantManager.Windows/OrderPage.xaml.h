8818980c333ea21154fcf8270738dfb3670f39bf
Venu  S Dharan <venusdharan@gmail.com>
2015-10-13 01:09:50 -0700

    added for edx

---
+++ RestaurantManager/RestaurantManager/RestaurantManager.Windows/OrderPage.xaml.h
@@ -0,0 +1,21 @@
+﻿//
+// OrderPage.xaml.h
+// Declaration of the OrderPage class
+//
+
+#pragma once
+
+#include "OrderPage.g.h"
+
+namespace RestaurantManager
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	[Windows::Foundation::Metadata::WebHostHidden]
+	public ref class OrderPage sealed
+	{
+	public:
+		OrderPage();
+	};
+}
